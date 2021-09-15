55fb175a128fe11af7fcc9fb76238d1279e5ab72
Will <willmdbaker@gmail.com>
2021-09-15 19:06:07 +0100

    Added Project Checking

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,50 @@
-import { useParams } from "react-router-dom"
-import { useEffect } from "react";
+import { useParams, Redirect } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { getStorage, ref, getDownloadURL, list, listAll } from "firebase/storage";
+
 
 const Project = () => {
 
     const params = useParams()
+    const storage = getStorage()
+
+    const [isProject, setIsProject] = useState(true)
+
 
     useEffect(() => {
-        console.log(params);
+        checkProject()
     }, []);
 
+    const checkProject = async () => {
+
+        const listRef = ref(storage, `Projects/${params.name}`)
+
+        const storageData = await listAll(listRef)
+            .then(async (res) => {
+                // const projectList = []
+                // res.prefixes.forEach((folderRef) => {
+                //     // console.log(folderRef.fullPath.replace(/Projects\//gm, ''))
+                //     projectList.push(folderRef.fullPath.replace(/Projects\//gm, ''))
+                // });
+                // return projectList
+                setIsProject(res.prefixes.length == 0 && res.items.length == 0 ? false : true)
+
+
+            })
+
+        // console.log('PROJECT LIST ==========>', storageData)
+
+        // return storageData
+
+    }
+
+
+
     return (
-        <h4>{params.name}</h4>
+        <>
+            <h4>{params.name}</h4>
+            {isProject ? <h4>This is a project!</h4> : <Redirect to='/' />}
+        </>
     )
 }
 
