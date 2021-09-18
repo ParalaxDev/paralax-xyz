50831c41b0e1ab2be62b12a04086dc67fa612f1a
Will <willmdbaker@gmail.com>
2021-09-18 10:43:47 +0100

    basic landing page animation

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,42 @@
+import { useEffect, useState } from 'react'
 import '../styles/homepage.scss'
 import Blob1 from '../svg/Blob1'
 
 const Homepage = () => {
 
+    const [title, setTitle] = useState('P.')
+    const [blobStates, setBlobStates] = useState('hidden')
+    const [taglineState, setTaglineState] = useState('hidden')
+
+    useEffect(() => {
+        loadingAnimation()
+    }, [])
+
+    const loadingAnimation = () => {
+
+        setTimeout(() => {
+            setTitle('Paralax.')
+            console.log('aniamtion plays')
+        }, 1500)
+
+        setTimeout(() => {
+            setTaglineState('fade-in-top')
+        }, 2000)
+
+        setTimeout(() => {
+            setBlobStates('fade-in-top')
+        }, 2500)
+    }
+
     return (
         <>
-            <div className='blob-container'>
+            <div className={`blob-container ${blobStates}`}>
                 <Blob1 className='blob top-right' />
                 <Blob1 className='blob bottom-left' />
             </div>
             <div className='title-container'>
-                <h1 className='title'>Paralax.</h1>
-                <h3 className='tagline'>Computer Science <span className='bold'>//</span> Graphic Design</h3>
+                <h1 className='title fade-in-bottom'>{title}</h1>
+                <h3 className={`tagline ${taglineState}`}>Computer Science <span className='bold'>//</span> Graphic Design</h3>
             </div>
         </>
     )
