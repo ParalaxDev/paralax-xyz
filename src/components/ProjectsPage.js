aab3e3d83a61fc426d35003828563033b5553453
Will <willmdbaker@gmail.com>
2021-10-12 19:33:00 +0100

    Fixed styling bugs

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -1,7 +1,7 @@
 import Heading from './Heading'
 import ListObject from './ListObject'
 
-
+import '../styles/basicStyling.scss'
 
 
 
@@ -10,11 +10,12 @@ const ProjectsPage = ({ }) => {
         <>
 
             <div style={{ height: '5vw' }} />
-            <Heading title='WORK' subtitle='ALL PROJECTS' />
-            <ListObject link='/projects/drbarkers' year="'21" title='Dr Barkers' description='GCSE Brief' />
-            <ListObject link='/projects/kew' year="'21" title='Kew Gardens' description='GCSE Brief' />
-            <ListObject link='/projects/typography' year="'20" title="Typography" description='GCSE Project' />
-
+            <div className='content-container'>
+                <Heading title='WORK' subtitle='ALL PROJECTS' />
+                <ListObject link='/projects/drbarkers' year="'21" title='Dr Barkers' description='GCSE Brief' />
+                <ListObject link='/projects/kew' year="'21" title='Kew Gardens' description='GCSE Brief' />
+                <ListObject link='/projects/typography' year="'20" title="Typography" description='GCSE Project' />
+            </div>
         </>
     )
 }
