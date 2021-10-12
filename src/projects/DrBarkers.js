7baea11b24a2f36fef4e06ae93c221bae61c1186
Will <willmdbaker@gmail.com>
2021-10-12 20:29:49 +0100

    small changes

--- src/projects/DrBarkers.js
+++ src/projects/DrBarkers.js
@@ -10,9 +10,15 @@ const DrBarkers = () => {
         <>
             <ScrollingHeader title='DR BARKERS' image={titleThumbnail} />
             <div className='content-container'>
-                <Heading title='BRIEF' subtitle='THE EXAM BRIEF' />
+                <section id='brief'>
+                    <h1>The Brief</h1>
+                    <p>A manufacturer requires graphics for a new brand of pet food named 'Dr Barker's'. You might consider approaches such branding and packaging or advertising material. Examples can be seen in packaging designs and in advertising for pet food by Ideas that Kick, 29 Agency, Seymourpowell Ltd, and Break Packaging and Design. When producing your response, carefully consider shelf presence and the target market.</p>
+                </section>
+                <section id='statementofintent'>
+                    <h1>Statement Of Intent</h1>
+                    <p>A manufacturer requires graphics for a new brand of pet food named 'Dr Barker's'. You might consider approaches such branding and packaging or advertising material. Examples can be seen in packaging designs and in advertising for pet food by Ideas that Kick, 29 Agency, Seymourpowell Ltd, and Break Packaging and Design. When producing your response, carefully consider shelf presence and the target market.</p>
+                </section>
                 <div style={{ height: '1000vh' }} />
-
             </div>
         </>
     )
